cfabfd86ff43f1e8dfd180d141194c3abcb8a521
PedroHLV <pedrolopesv@outlook.com>
2024-02-01 23:32:43 -0300

    firstCommit

---
+++ script.js
@@ -0,0 +1,60 @@
+const imagemVisualizacao = document.getElementById('imagem-visualizacao')
+const tituloProduto = document.getElementById('titulo-produto')
+
+const verdeCipreste = {
+    nome: 'Verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+};
+
+const azulInverno = {
+    nome: 'Azul-inverno',
+    pasta: 'imagens-azul-inverno'
+};
+
+const meiaNoite = {
+    nome: 'Meia-noite',
+    pasta: 'imagens-meia-noite'
+};
+
+const estelar = {
+    nome: 'Estelar',
+    pasta: 'imagens-estelar'
+};
+
+const rosaClaro = {
+    nome: 'Rosa-claro',
+    pasta: 'imagens-rosa-claro'
+};
+
+const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
+const opcoesTamanho = ['41 mm', '45 mm'];
+
+let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
+
+function trocarImagem() {
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    imagemVisualizacao.src = "./imagens/opcoes-cores/imagens-azul-inverno/imagem-" + imagemSelecionada + ".jpeg";
+};
+
+function trocarTamanho() {
+    // Atualizar variável de controle do tamanho da caixa
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+
+    // Mudar titulo do produto
+    tituloProduto.innerText = "Pulseira loop esportiva azul-inverno para caixa de " + opcoesTamanho[tamanhoSelecionado];
+
+    // Mudar o tamanho da imagem
+   if (opcoesTamanho[tamanhoSelecionado] === '41 mm') {
+    imagemVisualizacao.classList.add('caixa-pequena');
+   } else {
+    imagemVisualizacao.classList.remove('caixa-pequena');
+   }
+};
+
+function trocarCor() {
+    // Atualizar cor selecionada
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id;
+}
